e9bcde0fd0186e188514c0bed6afaaf2007a514f
fttmatteo <valenciaardila988@icloud.com>
2025-09-20 21:43:14 -0500

    modified class patient

--- clinic/src/main/java/app/domain/model/Patient.java
+++ clinic/src/main/java/app/domain/model/Patient.java
@@ -1,46 +1,111 @@
 package app.domain.model;
 
-public class Patient extends Person {
+public class Patient {
+    private int document;
+    private String fullName;
+    private String birthDate;
     private String gender;
+    private String address;
+    private int phoneNumber;
+    private String email;   
     private String emergencyFirstName;
     private String emergencyLastName;
     private String relationShip;
     private int emergencyPhone;
-    private MedicalInsurance insurancePolicy;
+    private MedicalInsurance insurancePolicy; //Preguntale al profesor
+
+    public int getDocument() {
+        return document;
+    }
+
+    public void setDocument(int document) {
+        this.document = document;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
 
     public String getGender() {
         return gender;
     }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getEmergencyFirstName() {
         return emergencyFirstName;
     }
+
     public void setEmergencyFirstName(String emergencyFirstName) {
         this.emergencyFirstName = emergencyFirstName;
     }
+
     public String getEmergencyLastName() {
         return emergencyLastName;
     }
+
     public void setEmergencyLastName(String emergencyLastName) {
         this.emergencyLastName = emergencyLastName;
     }
+
     public String getRelationShip() {
         return relationShip;
     }
+
     public void setRelationShip(String relationShip) {
         this.relationShip = relationShip;
     }
+
     public int getEmergencyPhone() {
         return emergencyPhone;
     }
+
     public void setEmergencyPhone(int emergencyPhone) {
         this.emergencyPhone = emergencyPhone;
     }
+
     public MedicalInsurance getInsurancePolicy() {
         return insurancePolicy;
     }
+
     public void setInsurancePolicy(MedicalInsurance insurancePolicy) {
         this.insurancePolicy = insurancePolicy;
     }
