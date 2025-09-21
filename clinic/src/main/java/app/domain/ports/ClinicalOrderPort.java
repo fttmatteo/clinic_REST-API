54d01e1884b14f70b7ab98687e0f849ce3721cc3
fttmatteo <valenciaardila988@icloud.com>
2025-09-20 23:52:00 -0500

    add clinicalorderport

---
+++ clinic/src/main/java/app/domain/ports/ClinicalOrderPort.java
@@ -0,0 +1,11 @@
+package app.domain.ports;
+
+import java.util.List;
+
+import app.domain.model.ClinicalOrder;
+
+public interface ClinicalOrderPort {
+    public ClinicalOrder findById (int numberOrder) throws Exception;
+    public List<ClinicalOrder> findByPatient (int patientDocument) throws Exception;
+    public ClinicalOrder save(ClinicalOrder clinicalOrder) throws Exception;
+}
