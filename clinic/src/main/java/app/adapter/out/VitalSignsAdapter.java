f0a424206ba267741ce2aa9fcc825813be38ee79
fttmatteo <valenciaardila988@icloud.com>
2025-09-28 12:46:28 -0500

    AD class in adapter - VitalSignsAdapter

---
+++ clinic/src/main/java/app/adapter/out/VitalSignsAdapter.java
@@ -0,0 +1,28 @@
+package app.adapter.out;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.domain.model.VitalSignsRecord;
+import app.domain.ports.VitalSignsPort;
+import app.infrastructure.persistence.entities.VitalSignsRecordEntity;
+import app.infrastructure.persistence.mapper.VitalSignsMapper;
+import app.infrastructure.persistence.repository.VitalSignsRepository;
+
+/**
+ * Adaptador de infraestructura que implementa el puerto
+ * {@link VitalSignsPort}. Permite guardar registros de signos vitales
+ * usando JPA.
+ */
+@Service
+public class VitalSignsAdapter implements VitalSignsPort {
+    @Autowired
+    private VitalSignsRepository vitalSignsRepository;
+
+    @Override
+    public void save(VitalSignsRecord record) throws Exception {
+        VitalSignsRecordEntity entity = VitalSignsMapper.toEntity(record);
+        vitalSignsRepository.save(entity);
+        record.setId(entity.getId());
+    }
+}
