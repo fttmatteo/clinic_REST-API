fdad0e6428ce388b6cbcf50b9e5908c799a3a8ad
fttmatteo <valenciaardila988@icloud.com>
2025-09-28 21:23:50 -0500

    AD class in validators - MedicineValidator

---
+++ clinic/src/main/java/app/adapter/in/validators/MedicineValidator.java
@@ -0,0 +1,23 @@
+package app.adapter.in.validators;
+
+import org.springframework.stereotype.Component;
+
+import app.application.exceptions.InputsException;
+
+/**
+ * Validador para campos de medicamentos. Amplía {@link SimpleValidator}
+ * proporcionando verificaciones específicas para identificador, nombre y
+ * costo.
+ */
+@Component
+public class MedicineValidator extends SimpleValidator {
+    public String idValidator(String value) throws InputsException {
+        return stringValidator("identificador del medicamento", value);
+    }
+    public String nameValidator(String value) throws InputsException {
+        return stringValidator("nombre del medicamento", value);
+    }
+    public Double costValidator(String value) throws InputsException {
+        return doubleValidator("costo del medicamento", value);
+    }
+}
