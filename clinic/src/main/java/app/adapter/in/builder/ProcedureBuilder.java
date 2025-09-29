d75817e5ba5dc66f37ad28235688687fa2c9d5ff
fttmatteo <valenciaardila988@icloud.com>
2025-09-28 21:27:00 -0500

    AD class in builder - ProcedureBuilder

---
+++ clinic/src/main/java/app/adapter/in/builder/ProcedureBuilder.java
@@ -0,0 +1,26 @@
+package app.adapter.in.builder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import app.adapter.in.validators.ProcedureValidator;
+import app.domain.model.inventory.Procedure;
+
+/**
+ * Builder para construir objetos {@link Procedure} a partir de datos
+ * proporcionados desde peticiones HTTP.  Usa {@link ProcedureValidator}
+ * para validar los campos.
+ */
+@Component
+public class ProcedureBuilder {
+    @Autowired
+    private ProcedureValidator validator;
+
+    public Procedure build(String id, String name, String cost) throws Exception {
+        Procedure procedure = new Procedure();
+        procedure.setId(validator.idValidator(id));
+        procedure.setName(validator.nameValidator(name));
+        procedure.setCost(validator.costValidator(cost));
+        return procedure;
+    }
+}
